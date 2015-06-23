1f209675fbb82c6a686dabdd209390b14378dc7e
TurnerMooreDavis <turnermdavis@gmail.com>
2015-06-23 14:55:10 -0400

    add additional views and form for email

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,4 +3,7 @@ class ReportsController < ApplicationController
     @assembly = Assembly.find_by_name(params[:name])
     @hits = Hit.where(subject_id: Gene.where(sequence_id: Sequence.where(assembly_id: @assembly.id))).order("percent_similarity DESC")
   end
+  def welcome
+    @data = params
+  end
 end
